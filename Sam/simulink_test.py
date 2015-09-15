0a99b8e09e794764e32d85c8137d2ffc78e96a1c
Scamwell <smscannell@gmail.com>
2015-09-15 20:10:26 +0100

    added comments

--- Sam/simulink_test.py
+++ Sam/simulink_test.py
@@ -1,1 +1,17 @@
-__author__ = 'Sam'
+# Connect to Crazyflie
+# Start logging baro and accZ
+# Send via UDP to simulink model
+# Receive commands from simulink
+
+from __future__ import print_function
+
+from threading import Thread
+import signal
+import time
+import sys
+
+try:
+    import zmq
+except ImportError as e:
+    raise Exception("ZMQ library probably not installed ({})".format(e))
+
